3830e5fa4aaf078d3f1a5743d997da64b9693e79
aalnoman10 <aalnomanm10@gmail.com>
2023-05-12 20:50:07 +0600

    add booking

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
         await client.connect();
 
         const serviceCollection = client.db('carDoctor').collection('services')
+        const bookingCollection = client.db('carDoctor').collection('bookings')
 
         app.get('/services', async (req, res) => {
             const finding = serviceCollection.find()
@@ -36,7 +37,21 @@ async function run() {
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
-            const result = await serviceCollection.findOne(query)
+
+            const options = {
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: { title: 1, title: 1, price: 1, service_id: 1, img: 1 },
+            };
+
+            const result = await serviceCollection.findOne(query, options)
+            res.send(result)
+        })
+
+        // bookingCollection
+
+        app.post('/bookings', async (req, res) => {
+            const customer = req.body;
+            const result = await bookingCollection.insertOne(customer)
             res.send(result)
         })
 
